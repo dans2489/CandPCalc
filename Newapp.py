70c3549d160c1706f100ce677afb0f5fe52a6277
dans2489 <141748818+dans2489@users.noreply.github.com>
2025-09-22 07:28:18 +0100

    Update Newapp.py

--- Newapp.py
+++ Newapp.py
@@ -47,7 +47,7 @@ table tr.total-row { font-weight: bold; background-color: #e6f0fa; }
 # ------------------------------
 # TITLE ONLY (NO LOGO)
 # ------------------------------
-st.title("Prison Workshop Costing Tool")
+st.title("Cost and Price Calculator")
 
 # ------------------------------
 # INPUTS
