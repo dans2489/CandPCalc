18195e77032b0050a5ab6b4e53c1dc4bf0749236
dans2489 <141748818+dans2489@users.noreply.github.com>
2025-09-22 07:22:42 +0100

    Update Newapp.py

--- Newapp.py
+++ Newapp.py
@@ -45,13 +45,7 @@ table tr.total-row { font-weight: bold; background-color: #e6f0fa; }
 """, unsafe_allow_html=True)
 
 # ------------------------------
-# LOGO + TITLE
-# ------------------------------
-col1, col2 = st.columns([1, 4])
-with col1:
-    st.image("logo.png", width=120)  # ensure logo.png is in same folder
-with col2:
-    st.title("Prison Workshop Costing Tool")
+    st.title("Cost and Price Calculator")
 
 # ------------------------------
 # INPUTS
